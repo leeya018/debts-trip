6a061a1e7331b94f95067b028bce56693635884b
leeya018 <leeyahav018@gmail.com>
2023-09-15 16:18:49 +0300

    s

---
+++ mobx/currencyStore.js
@@ -0,0 +1,134 @@
+import {
+  addDoc,
+  collection,
+  doc,
+  getDoc,
+  setDoc,
+  updateDoc,
+} from "firebase/firestore"
+import { db } from "../firebase"
+import axios from "axios"
+import { DB_CONNECTIONS, getUrl, getbaseAxios, tokenItem } from "lib/util"
+import { makeAutoObservable } from "mobx"
+import { messageStore } from "./messageStore"
+
+class Currency {
+  currencyTo = "USD"
+  currencyFrom = "EUR"
+
+  constructor() {
+    makeAutoObservable(this)
+    this.createGroup = this.createGroup.bind(this)
+    this.joinGroup = this.joinGroup.bind(this)
+    this.addProduct = this.addProduct.bind(this)
+    this.getMyList = this.getMyList.bind(this)
+  }
+
+  resetUserStore() {}
+
+  async createGroup(name, uid) {
+    try {
+      // Set the document
+      const docRef = await addDoc(collection(db, DB_CONNECTIONS.groups), {
+        name,
+        admin: uid,
+        users: [
+          {
+            uid,
+            items: [],
+          },
+        ],
+        created_date: new Date(),
+      })
+
+      // Get the document
+      console.log("Get the document")
+
+      const docSnapshot = await getDoc(docRef)
+
+      console.log(docSnapshot)
+      console.log(docSnapshot.id, docSnapshot.data())
+      const [id, data] = [docSnapshot.id, docSnapshot.data()]
+      this.group = { id, ...data }
+      console.log(this.group)
+      messageStore.setSuccess("Group created successfully")
+      return docSnapshot
+    } catch (error) {
+      console.log(error.message)
+      messageStore.setError(error.message)
+    }
+  }
+
+  async joinGroup(uid, groupId) {
+    const docRef = doc(db, DB_CONNECTIONS.groups, groupId)
+    console.log(docRef)
+    // Fetch the document
+    try {
+      const docSnapshot = await getDoc(docRef)
+      if (docSnapshot.exists()) {
+        // Get the current value of the array field
+        const users = docSnapshot.data().users
+
+        updateDoc(docRef, {
+          users: [
+            ...users,
+            {
+              uid,
+              items: [],
+            },
+          ],
+        })
+        return true
+      }
+      console.log("doc is not found")
+
+      return false
+    } catch (error) {
+      console.error("Error updating document: ", error)
+      messageStore.setError(error.message)
+    }
+  }
+
+  async addProduct(uid, groupId, product) {
+    const docRef = doc(db, DB_CONNECTIONS.groups, groupId)
+    console.log(docRef)
+    // Fetch the document
+
+    try {
+      const docSnapshot = await getDoc(docRef)
+      if (docSnapshot.exists()) {
+        // Get the current value of the array field
+        let users = docSnapshot.data().users
+        // let myProducts
+        users.map((user) => {
+          if (user.uid === uid) {
+            user.items.push(product)
+            this.myList.replace([...this.myList, product])
+          }
+          return user
+        })
+        await updateDoc(docRef, {
+          users,
+        })
+
+        console.log("added items successfully")
+        messageStore.setSuccess("added items successfully")
+        return true
+      } else {
+        throw new Error("user list is not here ")
+      }
+    } catch (error) {
+      console.error("Error adding item: ", error)
+      messageStore.setError(error.message)
+    }
+  }
+
+  async getMyList(uid, groupId) {
+    const docRef = doc(db, DB_CONNECTIONS.groups, groupId)
+    const docSnapshot = await getDoc(docRef)
+    const users = docSnapshot.data().users
+    const newList = users.find((user) => user.uid === uid)
+    this.myList.replace(newList)
+  }
+}
+export const currencyStore = new Currency()
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 
 import { useRouter } from "next/router"
 import Image from "next/image"
@@ -10,29 +10,60 @@ import { BiArrowBack } from "react-icons/bi"
 import { RiArrowUpDownFill } from "react-icons/ri"
 import { GoInfo } from "react-icons/go"
 import { TbArrowBackUp } from "react-icons/tb"
+import axios from "axios"
+import Freecurrencyapi from "@everapi/freecurrencyapi-js"
+import { currencyStore } from "mobx/currencyStore"
+const freecurrencyapi = new Freecurrencyapi(
+  "fca_live_Z1uEV83RkRvjDiDl3CF2a0vTC5JH2OZ72YMnbRyo"
+)
 export default function index() {
   const router = useRouter()
+  const [amount, setAmount] = useState(100)
+  const [result, setResult] = useState("")
 
-  useEffect(() => {}, [])
+  useEffect(() => {
+    freecurrencyapi
+      .latest({
+        base_currency: currencyStore.currencyFrom,
+        currencies: currencyStore.currencyTo,
+      })
+      .then((response) => {
+        console.log(response)
+        const rate = response.data[currencyStore.currencyTo]
+        setResult(rate * amount)
+      })
+  }, [amount])
 
   return (
     <div className="h-[100vh] w-screen bg-secondary flex flex-col  text-calc_white  ">
       {/* source currency */}
-      {/* <div className="h-36 flex justify-between items-center px-5">
-        <div className="flex flex-col justify-center items-center">
+      <div className="h-36 flex justify-between items-center px-5 bg-calc_gray_l ring-0">
+        <div className="flex flex-col justify-center items-center ">
           <Image
-            className="mr-2 rounded-full mb-2"
+            className=" rounded-full w-12 h-12"
             src={us.src}
             alt="israel"
             height={50}
             width={50}
           />
-          <div>isr</div>
+          <div>us</div>
         </div>
-        <input type="text" className="flex w-full h-full bg-calc_gray_l" />
-      </div> */}
+        <input
+          type="number"
+          value={amount}
+          onChange={(e) => {
+            setAmount((prev) => {
+              if (prev) {
+                return e.target.value
+              }
+              return 0
+            })
+          }}
+          className="flex w-full h-full bg-calc_gray_l"
+        />
+      </div>
       {/* target currency */}
-      {/* <div className="h-36 flex justify-between items-center px-5">
+      <div className="h-36 flex justify-between  items-center px-5 bg-calc_gray_l ring-0">
         <div className="flex flex-col justify-center items-center">
           <Image
             className="mr-2 rounded-full mb-2"
@@ -46,8 +77,12 @@ export default function index() {
           />
           <div>us</div>
         </div>
-        <input type="text" className="flex w-full h-full bg-calc_gray_l" />
-      </div> */}
+        <input
+          type="text"
+          value={result}
+          className="flex w-full h-full bg-calc_gray_l"
+        />
+      </div>
       {/* calculator */}
       <div className="flex-1 h-full grid grid-cols-4 grid-rows-4 ">
         <button className="w-full h-full flex justify-center items-center text-2xl bg-calc_gray_m">
@@ -66,15 +101,15 @@ export default function index() {
           {" "}
           /
         </button>
-        <button className="w-full h-full flex justify-center items-center text-2xl bg-calc_gray_m">
+        <button className="w-full h-full flex justify-center items-center text-2xl bg-calc_gray_s">
           {" "}
           7
         </button>
-        <button className="w-full h-full flex justify-center items-center text-2xl bg-calc_gray_m">
+        <button className="w-full h-full flex justify-center items-center text-2xl bg-calc_gray_s">
           {" "}
           8
         </button>
-        <button className="w-full h-full flex justify-center items-center text-2xl bg-calc_gray_m">
+        <button className="w-full h-full flex justify-center items-center text-2xl bg-calc_gray_s">
           {" "}
           9
         </button>
