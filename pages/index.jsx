9003498286cd7ecb40df9d315f1cecba23cbe3be
leeya018 <leeyahav018@gmail.com>
2023-09-17 22:45:54 +0300

    titlest

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,17 @@
 import React from "react"
 import { observer } from "mobx-react-lite"
 import UpNav from "components/whatsapp/UpNav"
+import Title from "components/whatsapp/Title"
 
 const index = observer(() => {
   return (
     <div className="h-[100vh] w-screen bg-secondary flex flex-col  px-2">
       <UpNav />
-      {/* <Title>WA Bussiness</Title>
-    <Filter />
-    <Choose />
-    <ArchivePanel />
-    <BottomNav /> */}
+      <Title>WA Bussiness</Title>
+      {/* <Filter /> */}
+      {/* <Choose /> */}
+      {/* <ArchivePanel /> */}
+      {/* <BottomNav /> */}
     </div>
   )
 })
