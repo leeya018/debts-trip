8ad360aa86b6fdd156b0419e3ac3b0879c832dab
lee yahav <leeyahav018@gmail.com>
2023-09-21 08:34:10 +0300

    s

--- components/belives/FlickerText.jsx
+++ components/belives/FlickerText.jsx
@@ -4,7 +4,7 @@ function FlickerText() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-900">
       <span
-        className="text-9xl font-bold text-red-400 shadow-2xl p-10 bg-gray-800 rounded-md animate-flicker duration-10000"
+        className="text-9xl font-bold text-white shadow-2xl p-10 bg-gray-800 rounded-md"
         style={{
           animation: "showHide 3s linear 0s 3 forwards",
         }}
